06055868ec6caa10a4e796af6e81ad62e5a10d91
Deep Guchhait <deepguchhait2921@gmail.com>
2025-04-23 21:27:25 +0530

    checkout page

---
+++ frontend/src/components/Cart/Checkout.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import RazorpayButton from './RazorpayButton';
+
+const cart = {
+    products: [
+        {
+            name: "Stylish Jacket",
+            size: "M",
+            color: "black",
+            price: 120,
+            image: "https://picsum.photos/150?random=1"
+        },
+        {
+            name: "Casul Sneakers",
+            size: "42",
+            color: "White",
+            price: 75,
+            image: "https://picsum.photos/150?random=2"
+        },
+    ],
+    totalPrice: 195,
+};
+
+const Checkout = () => {
+    const navigate = useNavigate();
+    const [checkoutId, setCheckoutId] = useState(null);
+    const [ShippingAddress, SetShippingAddress] = useState({
+        firstName: "",
+        lastName: "",
+        address: "",
+        city: "",
+        postalCode: "",
+        phone: "",
+
+    });
+
+const handleCreateCheckout = (e) => {
+    e.preventDefault();
+    //setCheckoutId(123)
+};
+
+const handlePaymentSuccess = (details) => {
+    console.log("Payment Successful", details);
+    navigate("/order-confirmation");
+    
+}
+
+  return (
+    <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter'>
+        {/**Left Section */}
+        <div className="p-6 bg-lime-50 border shadow-md rounded-lg ">
+            <h2 className="text-2xl uppercase mb-6">Checkout</h2>
+            <form onSubmit={handleCreateCheckout}>
+                <h3 className="text-lg mb-4">Contact Details</h3>
+                <div className="mb-4">
+                    <label className='block text-gray-800 '>Email</label>
+                    <input type="email" value="sdsj.help@gmail.com" className='w-full p-2 border rounded' disabled/>
+                </div>
+                <h3 className="text-lg mb-4">Delivery</h3>
+                <div className="mb-4 grid grid-cols-2 gap-4">
+
+                    <div>
+                        <label className="block text-gray-800">First Name</label>
+                        <input type="text" 
+                               value={ShippingAddress.firstName}
+                               onChange={(e) => SetShippingAddress({...ShippingAddress, firstName: e.target.value,})}
+                               className='w-full p-2 border rounded' required/>
+                    </div>
+
+                    <div>
+                        <label className="block text-gray-800">Last Name</label>
+                        <input type="text" 
+                               value={ShippingAddress.lastName}
+                               onChange={(e) => SetShippingAddress({...ShippingAddress, lastName: e.target.value,})}
+                               className='w-full p-2 border rounded' required/>
+                    </div>
+
+                </div>
+
+                <div className="mb-4">
+                    <label className="block text-gray-800">Address</label>
+                    <input type="text" value={ShippingAddress.address} 
+                           onChange={(e) => SetShippingAddress({...ShippingAddress, address: e.target.value,})} 
+                           className='w-full p-2 border rounded' required />
+                </div>
+
+                <div className="mb-4 grid grid-cols-2 gap-4">
+                <div>
+                        <label className="block text-gray-800">City</label>
+                        <input type="text" 
+                               value={ShippingAddress.city}
+                               onChange={(e) => SetShippingAddress({...ShippingAddress, city: e.target.value,})}
+                               className='w-full p-2 border rounded' required/>
+                    </div>
+
+                    <div>
+                        <label className="block text-gray-800">Pin Code</label>
+                        <input type="text" 
+                               value={ShippingAddress.postalCode}
+                               onChange={(e) => SetShippingAddress({...ShippingAddress, postalCode: e.target.value,})}
+                               className='w-full p-2 border rounded' required/>
+                    </div>
+                </div>
+
+                <div className="mb-4">
+                    <label className="block text-gray-800">Phone</label>
+                    <input type="tel" value={ShippingAddress.phone} 
+                           onChange={(e) => SetShippingAddress({...ShippingAddress, phone: e.target.value,})} 
+                           className='w-full p-2 border rounded' required />
+                </div>
+
+                <div className="mt-6">
+                    {!checkoutId ? (
+                        <button type='submit' className='w-full bg-black text-white py-3 rounded'>Continue to Payment</button>
+                    ) : (
+                        <div>
+                            <h3 className="text-lg mb-4 ">Pay with Razorpay</h3>
+                            {/**payment button component */}
+                            <RazorpayButton amount={100} onSuccess={handlePaymentSuccess} onError={(err) => alert("Payment failed. Try again.")}/>
+                        </div>
+                    )}
+                </div>
+
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Checkout
---
+++ frontend/src/components/Cart/RazorpayButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RazorpayButton = () => {
+  return (
+    <div>RazorpayButton</div>
+  )
+}
+
+export default RazorpayButton
