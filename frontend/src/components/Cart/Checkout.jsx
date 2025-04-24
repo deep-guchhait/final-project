adfc091ffe110cd2f6e4e1dbe14c6ad8b4ce0d6f
Deep Guchhait <deepguchhait2921@gmail.com>
2025-04-24 21:10:20 +0530

    order confirm page

--- frontend/src/components/Cart/Checkout.jsx
+++ frontend/src/components/Cart/Checkout.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import RazorpayButton from './RazorpayButton';
+import { Link, useNavigate } from 'react-router-dom';
+
+
 
 const cart = {
     products: [
@@ -37,14 +38,14 @@ const Checkout = () => {
 
 const handleCreateCheckout = (e) => {
     e.preventDefault();
-    //setCheckoutId(123)
+    setCheckoutId(123)
 };
 
-const handlePaymentSuccess = (details) => {
-    console.log("Payment Successful", details);
-    navigate("/order-confirmation");
+// const handlePaymentSuccess = (details) => {
+//     console.log("Payment Successful", details);
+//     navigate("/order-confirmation");
     
-}
+// }
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter'>
@@ -115,15 +116,43 @@ const handlePaymentSuccess = (details) => {
                         <button type='submit' className='w-full bg-black text-white py-3 rounded'>Continue to Payment</button>
                     ) : (
                         <div>
-                            <h3 className="text-lg mb-4 ">Pay with Razorpay</h3>
+                            <Link to="checkout/order-confirm" className="w-full bg-blue-600 text-white py-3 px-4 rounded hover:bg-blue-700 transition">Pay Now</Link>
                             {/**payment button component */}
-                            <RazorpayButton amount={100} onSuccess={handlePaymentSuccess} onError={(err) => alert("Payment failed. Try again.")}/>
+                            
                         </div>
                     )}
                 </div>
 
             </form>
         </div>
+
+
+        {/**Right Section */}
+        <div className='bg-orange-50 p-6 border shadow-md rounded-lg'>
+            <h3 className='text-lg mb-4'>Order Summary</h3>
+            <div className="border-t py-4 mb-4">{cart.products.map((product, index) => (
+                <div key={index} className='flex items-center justify-between py-2 border-b'>
+                    <div className="flex items-start">
+                        <img src={product.image} alt={product.name} className='w-20 h-24 object-cover mr-4' />
+                        <div>
+                            <h3 className='text-md'>{product.name}</h3>
+                            <p className="text-gray-600">Size: {product.size}</p>
+                            <p className="text-gray-600">Color: {product.color}</p>
+                        </div>
+                    </div>
+                    <p className="text-xl">₹{product.price ?.toLocaleString()}</p>
+                </div>
+            ))}
+            </div>
+            <div className="flex justify-between items-center text-lg mb-4">
+                <p>Total</p>
+                <p>₹{cart.totalPrice?.toLocaleString()}</p>
+            </div>
+            <div className="flex justify-between items-center text-lg">
+                
+            </div>
+        </div>
+
     </div>
   )
 }
--- frontend/src/components/Cart/RazorpayButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RazorpayButton = () => {
-  return (
-    <div>RazorpayButton</div>
-  )
-}
-
-export default RazorpayButton
