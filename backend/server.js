642b0a8b7f9f3115dfd8d4b26997094fdd57f336
Deep Guchhait <deepguchhait2921@gmail.com>
2025-05-13 19:12:30 +0530

    completed the cart and admin route

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,11 @@ const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const productAdminRoutes = require("./routes/productAdminRoutes");
+const adminOrderRoutes = require("./routes/adminOrderRoutes");
+
 
 const app = express();
 app.use(express.json());
@@ -30,7 +35,12 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
+// Admin
+app.use("/api/admin/users", adminRoutes);
+app.use("/api/admin/products", productAdminRoutes);
+app.use("/api/admin/orders", adminOrderRoutes)
 
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
