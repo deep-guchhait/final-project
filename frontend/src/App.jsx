5f5f2c649e6cfc22c3fd104786a2523254ff1135
Deep Guchhait <deepguchhait2921@gmail.com>
2025-05-14 02:10:03 +0530

    Product api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,8 +19,12 @@ import ProductManagement from './components/Admin/ProductManagement'
 import EditProductPage from './components/Admin/EditProductPage'
 import OrderManagement from './components/Admin/OrderManagement'
 
+import { Provider } from "react-redux";
+import store from './redux/store';
+
 const App = () => {
   return (
+    <Provider store={store}>
     <BrowserRouter>
     <Toaster position="top-right"/>
     <Routes>
@@ -51,6 +55,7 @@ const App = () => {
 
     </Routes>
     </BrowserRouter>
+    </Provider>
   )
 }
 
